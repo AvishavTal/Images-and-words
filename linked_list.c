ffc70f49e7d1527631bc3217cfdff2ad4a22100f
sapirnr <sapirneri@gmail.com>
2022-03-01 17:08:42 +0200

    try

--- linked_list.c
+++ linked_list.c
@@ -15,7 +15,7 @@ typedef union data{
 }data;
 
 typedef struct node{
-    void *data;// Any data type can be stored in this node
+    symbol data;// Any data type can be stored in this node
     struct node *next;
 }node;
 
@@ -23,24 +23,14 @@ typedef struct node{
 /* Function to add a node at the beginning of Linked List.
    This function expects a pointer to the data to be added
    and size of the data type */
-void push(node** head_ref, void *new_data, size_t data_size)
+void push(node** head_ref, symbol new_data)
 {
     // Allocate memory for node
     struct node* new_node = (node*)malloc(sizeof(struct node));
 
-    // Allocate memory for data
     new_node->data  = new_data;
-    //new_node->data  = malloc(data_size);
     new_node->next = (*head_ref);
-
-    // Copy contents of new_data to newly allocated memory.
-    // Assumption: char takes 1 byte.
-    //int i;
-    //for (i=0; i<data_size; i++)
-     //   *(char *)(new_node->data + i) = *(char *)(new_data + i);
-
-    // Change head pointer as new node is added at the beginning
-    (*head_ref)    = new_node;
+    (*head_ref) = new_node;
 }
 
 int main() {
@@ -51,37 +41,8 @@ int main() {
     printf("2\n");
     update_symbol(new_symbol, name1, 5, 4, 3, NULL);
     printf("3\n");
-    push(&start, new_symbol, sizeof(symbol));
-
-    printf("4");
-    //printf("start data = %s\n",start->data);
-    //printf("name = %l\n", get_value(start->data));
+    push(&start, new_symbol);
+    printf("4\n");
 
     return 0;
 }
-
-/* Function to print nodes in a given linked list. fpitr is used
-   to access the function to be used for printing current node data.
-   Note that different data types need different specifier in printf() */
-/*
-void printList(node *node, void (*fptr)(void *))
-{
-    while (node != NULL)
-    {
-        (*fptr)(node->data);
-        node = node->next;
-    }
-}
-
-// Function to print an integer
-void printInt(void *n)
-{
-    printf(" %d", *(int *)n);
-}
-
-// Function to print a float
-void printFloat(void *f)
-{
-    printf(" %f", *(float *)f);
-}
-*/
