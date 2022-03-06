36ddcef4a1bf1d830862f3b80bdc46058e264a37
Avishav Tal <avishavtal@gmail.com>
2022-03-06 18:08:43 +0200

    one input file added macro body changed to be of type list

--- macro.c
+++ macro.c
@@ -3,17 +3,18 @@
 //
 
 #include "macro.h"
+#include "linked_list.h"
 #include <stdlib.h>
 #define MEM_SIZE 8192
 struct macro{
     char *name;
-    char **body;
+    list body;
 };
 
 macro init_macro(){
     macro new_macro = (macro)malloc(sizeof(struct macro));
     new_macro->name = NULL;
-    new_macro->body = NULL;
+    new_macro->body = create_empty_list();
     return new_macro;
 }
 
@@ -24,20 +25,20 @@ void set_macro_name(macro macro, char *name){
     macro->name=name;
 }
 
-char **get_macro_body(macro macro){
+list get_macro_body(macro macro){
     return macro->body;
 }
-void set_macro_body(macro macro, char **body){
+void set_macro_body(macro macro, list body){
     macro->body=body;
 }
 void append_line(macro macro, char *line){
 
 }
-
+/*
 char *next_line(macro macro){//todo
     return *macro->body;
 }
-
+*/
 void free_macro(macro macro){
     free(macro->name);
     free(macro->body);
--- macro.h
+++ macro.h
@@ -4,12 +4,15 @@
 
 #ifndef EX14_MACRO_H
 #define EX14_MACRO_H
+
+#include "linked_list.h"
+
 typedef struct macro *macro;
 macro init_macro();
 char *get_macro_name(macro macro);
 void set_macro_name(macro macro, char *name);
-char **get_macro_body(macro macro);
-void set_macro_body(macro macro, char **body);
+list get_macro_body(macro macro);
+void set_macro_body(macro macro, list body);
 void append_line(macro macro, char *line);
 char *next_line(macro macro);
 #endif //EX14_MACRO_H
