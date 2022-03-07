7e1303a45ee18089b1fef4dac682f6a9ba309cf1
Avishav Tal <avishavtal@gmail.com>
2022-03-07 15:33:32 +0200

    first working version of pre_assembler

--- macro_table.c
+++ macro_table.c
@@ -2,15 +2,33 @@
 // Created by avishav on 2.3.2022.
 //
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "macro_table.h"
+#include "linked_list.h"
 struct macro_table{
-
+    list table;
 };
 macro get_macro_by_name(macro_table table, char *name) {
+    node current_node= get_head(table->table);
+    macro current_macro;
+    while (current_node){
+        current_macro= get_node_data(current_node);
+        if(!strcmp(name, get_macro_name(current_macro))){
+            return current_macro;
+        }
+        current_node= get_next_node(current_node);
+    }
     return NULL;
 }
 void push_macro(macro_table table, macro new_macro) {
+    add_to_tail(table->table,new_macro);
+}
+macro_table init_macro_table(){
+    macro_table result=(macro_table) malloc(sizeof(struct macro_table));
+    result->table=create_empty_list();
+    return result;
+}
+void delete_table(){
 
 }
-macro_table init_macro_table();
-void delete_table();
