e378be833851339cce5553f40fb123d9a476d67b
Avishav Tal <avishavtal@gmail.com>
2022-03-13 16:12:31 +0200

    some errors added

--- operator_table.h
+++ operator_table.h
@@ -4,11 +4,14 @@
 
 #ifndef EX14_OPERATOR_TABLE_H
 #define EX14_OPERATOR_TABLE_H
+#include "addressing_mode.h"
 typedef struct operator *operator;
 operator get_operator_by_mame(char *name);
 int get_opcode(operator op);
 int get_funct(operator op);
 int get_n_operands(operator op);
+int is_legal_dest_addressing_mode(operator op,addressing_mode mode);
+int is_legal_source_addressing_mode(operator op,addressing_mode mode);
 /**
  * check  opname is name of legal operand, if true then funct and opcode get the value of the opname
  * @param opname
