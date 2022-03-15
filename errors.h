a060c1f63f1a33189b0b475c2721f2097f535959
sapirnr <sapirneri@gmail.com>
2022-03-15 12:10:03 +0200

    errors.h final updates

--- errors.h
+++ errors.h
@@ -4,10 +4,12 @@
 
 #ifndef EX14_ERRORS_H
 #define EX14_ERRORS_H
+
 #include<stdio.h>
 typedef enum {NOT_ERROR,UNDEFINED_OPERATOR,DOUBLE_DEFINITION_OF_LABEL,ILLEGAL_SYMBOL_NAME,
               TOO_MANY_ARGS,TOO_FEW_ARGS,ILLEGAL_ADDRESSING,INVALID_REGISTER_NAME,NOT_INT,NOT_IN_RANGE_IMMEDIATE,UNDEFINED_SYMBOL,
               TOO_MANY_CHARS,ILLEGAL_DATA,ILLEGAL_STRING,SYNTAX_ERROR,INVALID_REGISTER_FOR_INDEX,ILLEGAL_OPERAND}error;
+
  /**
   * This function gets line's number and error code and print which error happened in which line
   * @param line_number the line's number where the error occurred
@@ -15,4 +17,4 @@ typedef enum {NOT_ERROR,UNDEFINED_OPERATOR,DOUBLE_DEFINITION_OF_LABEL,ILLEGAL_SY
   */
  void print_error(unsigned long line_number,error err_code);
 
-#endif //EX14_ERRORS_H
+#endif /* EX14_ERRORS_H */
