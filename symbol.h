04bdad9469154d536907d809c6d0f841f3d66494
Avishav Tal <avishavtal@gmail.com>
2022-03-15 11:23:32 +0200

    value, base_address and offset changed to unsigned long

--- symbol.h
+++ symbol.h
@@ -8,17 +8,17 @@
 
 typedef struct symbol *symbol;
 symbol init_symbol();
-symbol init_symbol_with_values(char *name,long value,long base_address,long offset,int is_entry,int is_extern,int is_data,int is_code);
-void update_symbol(symbol symbol, char* name, long value, long base_address, long offset);
+symbol init_symbol_with_values(char *name, unsigned long value, unsigned long base_address, unsigned long offset, int is_entry, int is_extern, int is_data, int is_code);
+void update_symbol(symbol symbol, char* name, unsigned long value, unsigned long base_address, unsigned long offset);
 char* get_symbol_name(symbol symbol);
 void set_symbol_name(symbol symbol , char *name);
 void delete_symbol(symbol to_delete);
-long get_symbol_value(symbol symbol);
-void set_symbol_value(symbol symbol , long value);
-long get_symbol_base_address(symbol symbol);
-void set_symbol_base_address(symbol symbol , long base_address);
-long get_symbol_offset(symbol symbol);
-void set_symbol_offset(symbol symbol , long offset);
+unsigned long get_symbol_value(symbol symbol);
+void set_symbol_value(symbol symbol , unsigned long value);
+unsigned long get_symbol_base_address(symbol symbol);
+void set_symbol_base_address(symbol symbol , unsigned long base_address);
+unsigned long get_symbol_offset(symbol symbol);
+void set_symbol_offset(symbol symbol , unsigned long offset);
 //void set_symbol_attribute(symbol symbol , attribute attribute);
 void mark_entry(symbol to_mark);
 void mark_extern(symbol to_mark);
