02043990e2d51b9f8b4d7e33396851673971cad1
sapirnr <sapirneri@gmail.com>
2022-03-15 18:39:22 +0200

    system_errors.c final updates

--- system_errors.c
+++ system_errors.c
@@ -4,14 +4,23 @@
 
 #include <stdlib.h>
 #include "boolean.h"
+#include "file.h"
 #include "system_errors.h"
 
-#define EXIT_CODE -1
+#define ALLOCATION_EXIT_CODE -1
+#define OPEN_FILE_EXIT_CODE -2
 
 boolean is_allocation_succeeded(void* pointer){
     if(pointer == NULL){
-        fprintf( stderr, "Unable to allocate memory for new node\n");
-        exit(EXIT_CODE);
+        fprintf( stderr, "Unable to allocate memory\n");
+        exit(ALLOCATION_EXIT_CODE);
     }
     return true;
+}
+
+boolean is_open_file_succeeded(file* curr_file) {
+    if(curr_file==NULL){
+        fprintf(stderr,"Unable to open file");
+        exit(OPEN_FILE_EXIT_CODE);
+    }
 }
