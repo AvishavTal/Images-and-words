76199c3e04ef2f52d190187f46adfc4e989a5be3
sapirnr <sapirneri@gmail.com>
2022-03-15 20:19:31 +0200

    string_manipulations.h final updates

--- string_manipulations.h
+++ string_manipulations.h
@@ -1,55 +1,61 @@
-//
-// Created by avishav on 2.3.2022.
-//
+/*
+* Created by Avishav & Sapir on March 2,2022
+*/
 
 #ifndef EX14_STRING_MANIPULATIONS_H
 #define EX14_STRING_MANIPULATIONS_H
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
+
 #include "boolean.h"
+
 /**
- * trim the whitespaces from the edges of the given string
- * @param str
- * @return
+ * this function trims whitespaces from the edges of the given string
+ * @param str the wanted string
+ * @return the string after trimming whitespaces from the edges
  */
 char *trim_whitespace(char *str);
+
 /**
  * split the given string to several strings
- * @param src the string to split
- * @param dest 2D array to hold the otput strings
- * @param delimiter
- * @return the number of strings
+ * @param src string to split
+ * @param dest 2D array to hold the output strings
+ * @param delimiter delimiter
+ * @return the number of separate strings
  */
 int split(char *src, char **dest, const char *delimiter);
 
+/**
+ * check if given string represent the number zero.
+ * @param str string to check
+ * @return true if str is representation of 0, false otherwise
+ */
+boolean is_zero(char *str);
 
 /**
  * convert string to a floating point number
  * @param str string to convert
- * @return 0 iff the string contains non numeric chars
+ * @return 0 if the string contains non numeric chars
  */
 int str_to_double(char *str, double *dest);
 
 /**
  * convert string to int
  * @param str string to convert
- * @return 0 iff the string contains non numeric chars
+ * @return 0 if the string contains non numeric chars
  */
 int str_to_int(char *str,int *dest);
 
 /**
- * check if given string represent the number zero.
- * @param str
- * @return 0 iff str is not representation of 0.
- */
-char is_zero(char *str);
-
-/**
-* return the first word in string without whitespaces
-* @param str
-* @return the first word in str without whitespaces
+* Return the first word in string with whitespaces
+* @param str the complete string
+* @return the first word in str with whitespaces
 */
 char* get_first_word_in_line(char *str);
+
+/**
+ * Check if all the characters in word are alphanumerics
+ * @param word work to check
+ * @return true if all the characters are alphanumeric, false otherwise
+ */
 boolean is_alpha_numeric_word(char *word);
-#endif //EX14_STRING_MANIPULATIONS_H
+
+#endif /* EX14_STRING_MANIPULATIONS_H */
