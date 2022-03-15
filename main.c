1716b677a415d368e9fe46c34b27f045a514bf20
Avishav Tal <avishavtal@gmail.com>
2022-03-15 17:04:38 +0200

    implementation of main

--- main.c
+++ main.c
@@ -3,12 +3,10 @@
 #include "linked_list.h"
 #include "symbol.h"
 #include "macro.h"
+#include "symbol_table.h"
 
 #define LINE_LENGTH 81
 
-
-void check_list_of_symbols();//to delete later
-void check_list_of_macros();//to delete later
 
 #include "file.h"
 #include "pre_assembler.h"
@@ -19,11 +17,11 @@ file *init_files(file *files, int n_files, char **names);
 
 void run_assembler(file *files, int n_files);
 
+
 int main(int argc, char **argv){
-//    FILE *ps= fopen("/home/avishav/Documents/study/cPrograming/seconde_chance/ex14/ps.as","r");
-//    FILE *dest=fopen("/home/avishav/Documents/study/cPrograming/seconde_chance/ex14/ps.out","a");
-    file ps= init_file("/home/avishav/Documents/study/cPrograming/seconde_chance/ex141/ps");
-    pre_assembler(ps);
+    file *files=NULL;
+    files= init_files(files,argc-1,argv+1);
+    run_assembler(files,argc-1);
     return 0;
 }
 
@@ -31,8 +29,19 @@ void run_assembler(file *files, int n_files) {
     int i=0;
     for (; i < n_files; ++i) {
         if (files[i]){
-
+            pre_assembler(files[i]);
+            if (has_passed_pre_assembler(files[i])){
+                first_scan(files[i]);
+                if (has_passed_first_scan(files[i])){
+                    second_scan(files[i]);
+                    if (has_passed_second_scan(files[i])){
+                        make_ent_file(files[i]);
+                        make_ext_file(files[i]);
+                    }
+                }
+            }
         }
+        tear_down(files[i]);
     }
 }
 
@@ -43,40 +52,4 @@ file *init_files(file *files, int n_files, char **names) {
         init_file(names[i]);
     }
     return files;
-}
-
-void check_list_of_symbols(){//to delete later
-    //list of symbols
-
-    list symbol_list = create_empty_list();
-    symbol new_symbol = init_symbol();
-    char *name1 = "symbol1";
-    update_symbol(new_symbol, name1, 5, 4, 3);
-    add_to_tail(symbol_list, new_symbol);
-
-    symbol new_symbol2 = init_symbol();
-    char *name2 = "symbol2";
-    update_symbol(new_symbol2, name2, 5, 4, 3);
-    add_to_tail(symbol_list, new_symbol2);
-
-    print_list(symbol_list);
-}
-
-void check_list_of_macros(){//to delete later
-    //list of symbols
-
-    list macro_list = create_empty_list();
-
-    macro new_macro = init_macro();
-    char *name1 = "macro1";
-    set_macro_name(new_macro, name1);
-
-    add_to_tail(macro_list, new_macro);
-
-    macro new_macro2 = init_macro();
-    char *name2 = "macro2";
-    set_macro_name(new_macro2, name2);
-
-    add_to_tail(macro_list, new_macro2);
-    print_list(macro_list);
 }
