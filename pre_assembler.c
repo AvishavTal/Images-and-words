a7be00b81c8ac9753ddb4e48da28177324e20303
Avishav Tal <avishavtal@gmail.com>
2022-03-20 14:37:08 +0200

    fixed bug in is_open_file_succeeded, and added param is_fatal. if is_fatal==true, than the program will exit. also param file_name added to print the name of the file that dose not opened

--- pre_assembler.c
+++ pre_assembler.c
@@ -3,6 +3,7 @@
 //
 #include <stdio.h>
 #include <string.h>
+#include "system_errors.h"
 #include "string_manipulations.h"
 #include "macro_table.h"
 #include "file.h"
@@ -19,36 +20,39 @@ void pre_assembler(file source) {
     char line[LINE_SIZE],temp_line[LINE_SIZE],*first_word_in_line;
     macro temp_macro;
     macro_table table= get_macro_table(source);
-    src= fopen(get_name_as(source),"r");//todo check if open file succeeded
-    dest= fopen(get_name_am(source),"w");
-
-    while ((fgets(line,LINE_SIZE,src)!=NULL)){
-        strcpy(temp_line,line);
-        first_word_in_line= strtok(temp_line," \t");
-        trim_whitespace(first_word_in_line);
-        if(!macro_definition){
-            if((temp_macro= get_macro_by_name(table, first_word_in_line)) != NULL){/*macro call*/
-                macro_call(dest,temp_macro);
-            } else if(start_of_macro_definition(first_word_in_line)){
-                char *name;
-                temp_macro=init_macro();
-                macro_definition=1;
-                name= strtok(NULL," \t");
-                set_macro_name(temp_macro, name);
-            } else{
-                write_line(dest,line);
+    src= fopen(get_name_as(source),"r");
+    if (is_open_file_succeeded(src,false, get_name_as(source))){
+        dest= fopen(get_name_am(source),"w");
+        if (is_open_file_succeeded(dest,true, get_name_am(source))){
+            while ((fgets(line,LINE_SIZE,src)!=NULL)){
+                strcpy(temp_line,line);
+                first_word_in_line= strtok(temp_line," \t");
+                trim_whitespace(first_word_in_line);
+                if(!macro_definition){
+                    if((temp_macro= get_macro_by_name(table, first_word_in_line)) != NULL){/*macro call*/
+                        macro_call(dest,temp_macro);
+                    } else if(start_of_macro_definition(first_word_in_line)){
+                        char *name;
+                        temp_macro=init_macro();
+                        macro_definition=1;
+                        name= strtok(NULL," \t");
+                        set_macro_name(temp_macro, name);
+                    } else{
+                        write_line(dest,line);
+                    }
+                } else{
+                    if(end_of_macro_definition(first_word_in_line)){
+                        push_macro(table, temp_macro);
+                        macro_definition=0;
+                    } else{
+                        append_line(temp_macro,line);
+                    }
+                }
             }
-        } else{
-            if(end_of_macro_definition(first_word_in_line)){
-                push_macro(table, temp_macro);
-                macro_definition=0;
-            } else{
-                append_line(temp_macro,line);
-            }
+            fclose(dest);
         }
+        fclose(src);
     }
-    fclose(dest);
-    fclose(src);
 }
 
 void write_line(FILE *dest, char *line) {
