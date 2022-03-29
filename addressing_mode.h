b9dc522ee7274aea784925694aa392f64e17aa8c
Avishav Tal <avishavtal@gmail.com>
2022-03-29 20:51:58 +0300

    documentation addressing_mode.h

--- addressing_mode.h
+++ addressing_mode.h
@@ -4,7 +4,9 @@
 
 #ifndef EX14_ADDRESSING_MODE_H
 #define EX14_ADDRESSING_MODE_H
-
+/**
+ * the addressing modes allowed in our assembly
+ */
 typedef enum {DEFAULT=0,IMMEDIATE=0,DIRECT=1,INDEX=2,REGISTER_DIRECT=3}addressing_mode;
 
 #endif
