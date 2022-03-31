239156868be0a9fa04855451951e09e7cb5bbb90
sapirnr <sapirneri@gmail.com>
2022-03-31 13:46:04 +0300

    ifndef added

--- first_and_second_scan_constants.h
+++ first_and_second_scan_constants.h
@@ -1,4 +1,11 @@
+/*
+* Created by Avishav & Sapir on March 14,2022
+*/
 
+#ifndef EX14_FIRST_AND_SECOND_SCAN_CONSTANTS_H
+#define EX14_FIRST_AND_SECOND_SCAN_CONSTANTS_H
 
 #define LINE_LENGTH 81
 #define MIN_IC 100
+
+#endif
