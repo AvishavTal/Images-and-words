c5c23e81cfbbb53580990945834f69464349965d
sapirnr <sapirneri@gmail.com>
2022-04-01 01:34:39 +0300

    FINAL

--- macro.h
+++ macro.h
@@ -9,12 +9,49 @@
 #include "linked_list.h"
 #include "macro_body.h"
 
+/**
+ * Representation of macro
+ */
 typedef struct macro *macro;
+
+/**
+ * Create new macro and reset all variables
+ * @return
+ */
 macro init_macro();
+
+/**
+ * Gets a macro and returns its name
+ * @param macro the needed macro
+ * @return macro name
+ */
 char *get_macro_name(macro macro);
+
+/**
+ * Gets a macro and requested name and update its name
+ * @param macro the needed macro
+ * @param name requested name
+ */
 void set_macro_name(macro macro, char *name);
-void macro_call(FILE *dest,macro to_print);
+
+/**
+ * Append line to the body of the macro
+ * @param macro the needed macro
+ * @param line new line
+ */
 void append_line(macro macro, char *line);
+
+/**
+ * free the memory of the macro
+ * @param to_delete the wanted macro to delete
+ */
 void delete_macro(macro to_delete);
+
+/**
+ * print macro for dest file
+ * @param dest destination file to write into
+ * @param to_print required macro to print
+ */
+void macro_call(FILE *dest,macro to_print);
 
 #endif
