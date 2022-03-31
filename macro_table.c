101c895e9381bd8669b9c45c0feb57d6c1a7f736
sapirnr <sapirneri@gmail.com>
2022-04-01 02:25:10 +0300

    FINAL

--- macro_table.c
+++ macro_table.c
@@ -8,19 +8,30 @@
 #include "linked_list.h"
 #include "system_errors.h"
 
-struct macro_table{
+struct macro_table {
     list table;
 };
 
+/* public functions implementation */
+
+macro_table init_macro_table() {
+    macro_table result;
+    result = (macro_table) malloc(sizeof(struct macro_table));
+    if (is_allocation_succeeded(result)) {
+        result->table = create_empty_list();
+    }
+    return result;
+}
+
 macro get_macro_by_name(macro_table macros, char *name) {
-    node current_node= get_head(macros->table);
-    macro current_macro,result=NULL;
-    while (current_node!=NULL && result==NULL){
-        current_macro= get_node_data(current_node);
-        if(!strcmp(name, get_macro_name(current_macro))){
-            result=current_macro;
+    node current_node = get_head(macros->table);
+    macro current_macro,result = NULL;
+    while ((current_node != NULL) && (result == NULL)) {
+        current_macro = get_node_data(current_node);
+        if (!strcmp(name, get_macro_name(current_macro))) {
+            result = current_macro;
         }
-        current_node= get_next_node(current_node);
+        current_node = get_next_node(current_node);
     }
     return result;
 }
@@ -28,21 +39,16 @@ macro get_macro_by_name(macro_table macros, char *name) {
 void push_macro(macro_table table, macro new_macro) {
     add_to_tail(table->table,new_macro);
 }
-macro_table init_macro_table(){
-    macro_table result;
-    result = (macro_table) malloc(sizeof(struct macro_table));
-    if(is_allocation_succeeded(result)) {
-        result->table = create_empty_list();
-    }
-    return result;
-}
+
 void delete_macro_table(macro_table to_delete) {
     node current;
     current = get_head(to_delete->table);
-    while (current){
+    while (current) {
         delete_macro((macro)get_node_data(current));
-        current=get_next_node(current);
+        current = get_next_node(current);
     }
     free_list(to_delete->table);
     free(to_delete);
 }
+
+/* end of public functions implementation */
