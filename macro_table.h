cc691df9bfc7f3034489c4db00899e456184d157
sapirnr <sapirneri@gmail.com>
2022-04-01 02:25:07 +0300

    FINAL

--- macro_table.h
+++ macro_table.h
@@ -7,10 +7,36 @@
 
 #include "macro.h"
 
+/**
+ * Representation of macro table
+ */
 typedef struct macro_table *macro_table;
+
+/**
+ * Creates new macro table and reset all variables
+ * @return new macro body
+ */
 macro_table init_macro_table();
-void delete_macro_table(macro_table to_delete);
+
+/**
+ * This function gets macro name and return the macro object
+ * * @param macros macro table to search in
+ * @param name macro required name
+ * @return macro object
+ */
 macro get_macro_by_name(macro_table macros, char *name);
+
+/**
+ * Gets macro and insert to macro table
+ * @param table macro table to update
+ * @param new_macro macro to insert
+ */
 void push_macro(macro_table table, macro new_macro);
+
+/**
+ * free the memory of all macros in macro table
+ * @param to_delete macro table to delete
+ */
+void delete_macro_table(macro_table to_delete);
 
 #endif
