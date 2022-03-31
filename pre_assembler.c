a9ae4324f4b5c2ea47e97dc859243dc82e930628
Avishav Tal <avishavtal@gmail.com>
2022-03-31 20:06:41 +0300

    while moved to read_lines function

--- pre_assembler.c
+++ pre_assembler.c
@@ -10,53 +10,53 @@
 #include "file.h"
 #include "parser.h"
 #include "general_constants.h"
+#include "boolean.h"
 
 
-void write_line(FILE *dest, char *line);
+void read_lines(file source, FILE *src, FILE *dest);
 
 void pre_assembler(file source) {
     FILE *src,*dest;
-    int macro_definition=0;
-    char line[LINE_LENGTH],temp_line[LINE_LENGTH],*first_word_in_line;
-    macro temp_macro;
-    macro_table table= get_macro_table(source);
     src= fopen(get_name_as(source),"r");
     if (!is_open_file_succeeded(src,false, get_name_as(source))) {
         mark_pre_assembler_failed(source);
     } else{
         dest= fopen(get_name_am(source),"w");
         if (is_open_file_succeeded(dest,true, get_name_am(source))){
-            while ((fgets(line,LINE_LENGTH,src)!=NULL)){
-                strcpy(temp_line,line);
-                first_word_in_line= strtok(temp_line," \t");
-                trim_whitespace(first_word_in_line);
-                if(!macro_definition){
-                    if((temp_macro= get_macro_by_name(table, first_word_in_line)) != NULL){/*macro call*/
-                        macro_call(dest,temp_macro);
-                    } else if(start_of_macro_definition(first_word_in_line)){
-                        char *name;
-                        temp_macro=init_macro();
-                        macro_definition=1;
-                        name= strtok(NULL," \t");
-                        set_macro_name(temp_macro, name);
-                    } else{
-                        write_line(dest,line);
-                    }
-                } else{
-                    if(end_of_macro_definition(first_word_in_line)){
-                        push_macro(table, temp_macro);
-                        macro_definition=0;
-                    } else{
-                        append_line(temp_macro,line);
-                    }
-                }
-            }
+            read_lines(source, src, dest);
             fclose(dest);
         }
         fclose(src);
     }
 }
 
-void write_line(FILE *dest, char *line) {
-    fprintf(dest,"%s",line);
+void read_lines(file source, FILE *src, FILE *dest) {
+    boolean is_macro_definition=false;
+    char line[LINE_LENGTH],*first_word_in_line;
+    macro temp_macro;
+    macro_table table= get_macro_table(source);
+    while ((fgets(line,LINE_LENGTH,src)!=NULL)){
+        first_word_in_line= str_tok(line," \t");
+        first_word_in_line=trim_whitespace(first_word_in_line);
+        if(!is_macro_definition){
+            if((temp_macro= get_macro_by_name(table, first_word_in_line)) != NULL){/*macro call*/
+                macro_call(dest,temp_macro);
+            } else if(start_of_macro_definition(first_word_in_line)){
+                char *name;
+                temp_macro=init_macro();
+                is_macro_definition=true;
+                name= str_tok(NULL," \t");
+                set_macro_name(temp_macro, name);
+            } else{
+                fprintf(dest,"%s",line);
+            }
+        } else{
+            if(end_of_macro_definition(first_word_in_line)){
+                push_macro(table, temp_macro);
+                is_macro_definition=false;
+            } else{
+                append_line(temp_macro,line);
+            }
+        }
+    }
 }
