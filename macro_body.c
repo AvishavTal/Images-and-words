4770798914d6e03ed40f6c4774315abee5d28422
sapirnr <sapirneri@gmail.com>
2022-04-01 01:34:36 +0300

    FINAL

--- macro_body.c
+++ macro_body.c
@@ -9,36 +9,38 @@
 #include "linked_list.h"
 #include "system_errors.h"
 
-struct body{
+struct body {
     list lines;
 };
 
-macro_body init_body(){
+/* public functions implementation */
+
+macro_body init_body() {
     macro_body result;
-    result = (macro_body) malloc(sizeof(struct body));
-    if(is_allocation_succeeded(result)) {
+    result = (macro_body)malloc(sizeof(struct body));
+    if (is_allocation_succeeded(result)) {
         result->lines = create_empty_list();
     }
     return result;
 }
 
-void delete_body(macro_body to_delete){
+void delete_body(macro_body to_delete) {
     node current;
-    current= get_head(to_delete->lines);
-    while (current!=NULL){
+    current = get_head(to_delete->lines);
+    while (current != NULL) {
         free(get_node_data(current));
-        current= get_next_node(current);
+        current = get_next_node(current);
     }
     free_list(to_delete->lines);
     free(to_delete);
 }
 
-void append_line_to_body(macro_body to_update,char *line){
+void append_line_to_body(macro_body to_update,char *line) {
     char *new_line;
     unsigned long len;
-    len=strlen(line)+1;
-    new_line = (char *) malloc(len);
-    if(is_allocation_succeeded(new_line)) {
+    len = strlen(line)+1;
+    new_line = (char*)malloc(len);
+    if (is_allocation_succeeded(new_line)) {
         strcpy(new_line, line);
         add_to_tail(to_update->lines, new_line);
     }
@@ -46,9 +48,11 @@ void append_line_to_body(macro_body to_update,char *line){
 
 void print_body(FILE *dest, macro_body to_print) {
     node current;
-    current= get_head(to_print->lines);
-    while (current){
+    current = get_head(to_print->lines);
+    while (current) {
         fprintf(dest,"%s",(char *) get_node_data(current));
-        current= get_next_node(current);
+        current = get_next_node(current);
     }
 }
+
+/* end of public functions implementation */
