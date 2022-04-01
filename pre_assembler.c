2fe88b9f46c112365042c5540eb589720b7fd0ee
sapirnr <sapirneri@gmail.com>
2022-04-01 05:04:31 +0300

    FINAL

--- pre_assembler.c
+++ pre_assembler.c
@@ -3,7 +3,6 @@
 */
 
 #include <stdio.h>
-#include <string.h>
 #include "system_errors.h"
 #include "string_manipulations.h"
 #include "macro_table.h"
@@ -12,51 +11,63 @@
 #include "general_constants.h"
 #include "boolean.h"
 
-
+/* private function declaration */
 void read_lines(file source, FILE *src, FILE *dest);
+/* end of private function declaration */
 
+
+
+/* public functions implementation */
 void pre_assembler(file source) {
-    FILE *src,*dest;
-    src= fopen(get_name_as(source),"r");
+    FILE *src, *dest;
+    src = fopen(get_name_as(source),"r");
     if (!is_open_file_succeeded(src,false, get_name_as(source))) {
         mark_pre_assembler_failed(source);
-    } else{
-        dest= fopen(get_name_am(source),"w");
-        if (is_open_file_succeeded(dest,true, get_name_am(source))){
+    } else {
+        dest = fopen(get_name_am(source),"w");
+        if (is_open_file_succeeded(dest,true, get_name_am(source))) {
             read_lines(source, src, dest);
             fclose(dest);
         }
         fclose(src);
     }
 }
+/* end of public functions implementation */
 
+
+
+/* private functions implementation */
+/*
+ * this function is all the logic of pre assembler
+ */
 void read_lines(file source, FILE *src, FILE *dest) {
-    boolean is_macro_definition=false;
-    char line[LINE_LENGTH],*first_word_in_line;
+    boolean is_macro_definition = false;
+    char line[LINE_LENGTH], *first_word_in_line;
     macro temp_macro;
-    macro_table table= get_macro_table(source);
-    while ((fgets(line,LINE_LENGTH,src)!=NULL)){
-        first_word_in_line= str_tok(line," \t");
-        first_word_in_line=trim_whitespace(first_word_in_line);
-        if(!is_macro_definition){
-            if((temp_macro= get_macro_by_name(table, first_word_in_line)) != NULL){/*macro call*/
+    macro_table table = get_macro_table(source);
+    while ((fgets(line,LINE_LENGTH,src) != NULL)) {
+        first_word_in_line = str_tok(line," \t");
+        first_word_in_line = trim_whitespace(first_word_in_line);
+        if (!is_macro_definition) {
+            if ((temp_macro = get_macro_by_name(table, first_word_in_line)) != NULL) {/*macro call*/
                 macro_call(dest,temp_macro);
-            } else if(start_of_macro_definition(first_word_in_line)){
+            } else if (start_of_macro_definition(first_word_in_line)) {
                 char *name;
-                temp_macro=init_macro();
-                is_macro_definition=true;
-                name= str_tok(NULL," \t");
+                temp_macro = init_macro();
+                is_macro_definition = true;
+                name = str_tok(NULL," \t");
                 set_macro_name(temp_macro, name);
-            } else{
+            } else {
                 fprintf(dest,"%s",line);
             }
-        } else{
-            if(end_of_macro_definition(first_word_in_line)){
+        } else {
+            if (end_of_macro_definition(first_word_in_line)) {
                 push_macro(table, temp_macro);
-                is_macro_definition=false;
-            } else{
+                is_macro_definition = false;
+            } else {
                 append_line(temp_macro,line);
             }
         }
     }
 }
+/* end of private functions implementation */
