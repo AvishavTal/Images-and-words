b561929b52d39eae389fd5c5648a2aa12fe56004
sapirnr <sapirneri@gmail.com>
2022-04-01 04:08:05 +0300

    FINAL

--- system_errors.c
+++ system_errors.c
@@ -9,8 +9,9 @@
 #define ALLOCATION_EXIT_CODE -1
 #define OPEN_FILE_EXIT_CODE -2
 
-boolean is_allocation_succeeded(void* pointer){
-    if(pointer == NULL){
+/* public functions implementation */
+boolean is_allocation_succeeded(void* pointer) {
+    if (pointer == NULL) {
         fprintf( stderr, "Unable to allocate memory\n");
         exit(ALLOCATION_EXIT_CODE);
     }
@@ -18,13 +19,14 @@ boolean is_allocation_succeeded(void* pointer){
 }
 
 boolean is_open_file_succeeded(FILE *curr_file, boolean is_fatal, const char *file_name) {
-    boolean result=true;
-    if(curr_file==NULL){
-        result=false;
+    boolean result = true;
+    if (curr_file == NULL) {
+        result = false;
         fprintf(stderr,"Unable to open file: %s\n",file_name);
-        if (is_fatal){
+        if (is_fatal) {
             exit(OPEN_FILE_EXIT_CODE);
         }
     }
     return result;
 }
+/* end of public functions implementation */
