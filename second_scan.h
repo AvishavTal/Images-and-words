5e654e3262872e2fe9e3fa30049460d639ad460f
sapirnr <sapirneri@gmail.com>
2022-04-01 04:34:55 +0300

    FINAL

--- second_scan.h
+++ second_scan.h
@@ -7,6 +7,13 @@
 
 #include "file.h"
 
+/**
+ * In the second scan we find the following:
+ * 1. Find second scan errors if exist and print them
+ * 2. check entry definition in symbols table
+ * 3. print all words to object file
+ * @param source information about file in the program
+ */
 void second_scan(file source);
 
 #endif
