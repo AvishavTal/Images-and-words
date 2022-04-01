547b23d61064b0698cfbb63333cb72f8f9e39f33
sapirnr <sapirneri@gmail.com>
2022-04-01 04:26:41 +0300

    FINAL

--- word.c
+++ word.c
@@ -24,92 +24,93 @@
 
 #define IMMEDIATE_MASK 0xffff
 
-struct word{
+struct word {
     unsigned long address;
     unsigned long the_actual_word;
 };
 
-word init_word(){
+
+/* public functions implementation */
+word init_word() {
     word result;
-    result=(word) malloc(sizeof(struct word));
-    if(is_allocation_succeeded(result)) {
+    result = (word)malloc(sizeof(struct word));
+    if (is_allocation_succeeded(result)) {
         result->the_actual_word = 0;
         result->address = 0;
     }
     return result;
 }
 
-
-void print_word(FILE *dest, word to_print){
+void print_word(FILE *dest, word to_print) {
     unsigned int a,b,c,d,e;
-    a=to_print->the_actual_word>>A_SHIFT;
-    b=to_print->the_actual_word>>B_SHIFT;
-    c=to_print->the_actual_word>>C_SHIFT;
-    d=to_print->the_actual_word>>D_SHIFT;
-    e=to_print->the_actual_word>>E_SHIFT;
-    a&=MASK;
-    b&=MASK;
-    c&=MASK;
-    d&=MASK;
-    e&=MASK;
+    a = to_print->the_actual_word>>A_SHIFT;
+    b = to_print->the_actual_word>>B_SHIFT;
+    c = to_print->the_actual_word>>C_SHIFT;
+    d = to_print->the_actual_word>>D_SHIFT;
+    e = to_print->the_actual_word>>E_SHIFT;
+    a &= MASK;
+    b &= MASK;
+    c &= MASK;
+    d &= MASK;
+    e &= MASK;
     fprintf(dest,"%04lu A%x-B%x-C%x-D%x-E%x\n",to_print->address,a,b,c,d,e);
 }
 
-
-void set_address(word to_set,unsigned long new_address){
-    to_set->address=new_address;
+void set_address(word to_set,unsigned long new_address) {
+    to_set->address = new_address;
 }
 
-unsigned long get_address(word word1){
-    return word1->address;
+unsigned long get_address(word word1) {
+    return (word1->address);
 }
 
-void delete_word(word to_delete){
+void delete_word(word to_delete) {
     free(to_delete);
 }
 
-void set_are(word to_set, are new_are){
+void set_are(word to_set, are new_are) {
     unsigned int temp;
-    temp=new_are;
+    temp = new_are;
     temp <<= ARE_SHIFT;
     to_set->the_actual_word |= temp;
 }
 
-void set_opcode(word to_set, unsigned int new_opcode){
-    new_opcode=1<<new_opcode;
+void set_opcode(word to_set, unsigned int new_opcode) {
+    new_opcode = 1 << new_opcode;
     to_set->the_actual_word |= new_opcode;
 }
 
-void set_funct(word to_set, unsigned int new_funct){
-    new_funct<<=FUNCT_SHIFT;
-    to_set->the_actual_word|=new_funct;
+void set_funct(word to_set, unsigned int new_funct) {
+    new_funct <<= FUNCT_SHIFT;
+    to_set->the_actual_word |= new_funct;
 }
 
-void set_dest_register(word to_set, unsigned int reg_num){
-    reg_num<<=DEST_REG_SHIFT;
-    to_set->the_actual_word|=reg_num;
+void set_dest_register(word to_set, unsigned int reg_num) {
+    reg_num <<= DEST_REG_SHIFT;
+    to_set->the_actual_word |= reg_num;
 }
 
-void set_dest_addressing(word to_set, addressing_mode dest_addressing){
+void set_dest_addressing(word to_set, addressing_mode dest_addressing) {
     unsigned long temp;
-    temp=dest_addressing;
-    temp<<=DEST_ADDRESSING_SHIFT;
-    to_set->the_actual_word|=temp;
+    temp = dest_addressing;
+    temp <<= DEST_ADDRESSING_SHIFT;
+    to_set->the_actual_word |= temp;
 }
 
-void set_src_register(word to_set, unsigned int reg_num){
-    reg_num<<=SRC_REG_SHIFT;
-    to_set->the_actual_word|=reg_num;
+void set_src_register(word to_set, unsigned int reg_num) {
+    reg_num <<= SRC_REG_SHIFT;
+    to_set->the_actual_word |= reg_num;
 }
 
-void set_src_addressing(word to_set, addressing_mode src_addressing){
+void set_src_addressing(word to_set, addressing_mode src_addressing) {
     unsigned long temp;
-    temp=src_addressing;
-    temp<<=SRC_ADDRESSING_SHIFT;
-    to_set->the_actual_word|=temp;
+    temp = src_addressing;
+    temp <<= SRC_ADDRESSING_SHIFT;
+    to_set->the_actual_word |= temp;
 }
 
-void set_immediate(word to_set, unsigned int immediate){
-    immediate&=IMMEDIATE_MASK;
-    to_set->the_actual_word|=immediate;
+void set_immediate(word to_set, unsigned int immediate) {
+    immediate &= IMMEDIATE_MASK;
+    to_set->the_actual_word |= immediate;
 }
+/* end of public functions implementation */
